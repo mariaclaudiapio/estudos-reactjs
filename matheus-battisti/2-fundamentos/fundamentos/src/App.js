2be5d49f379746544eb5ceb87df2c09cc3c2bfa0
mariaclaudiapio <mariaclaudiapio@gmail.com>
2022-12-22 13:40:46 -0300

    Fundamentos do React
    parte III

--- matheus-battisti/2-fundamentos/fundamentos/src/App.js
+++ matheus-battisti/2-fundamentos/fundamentos/src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react'
 
 // import FirstComponent from './components/FirstComponent'
 // import MyComponent from './components/MyComponent'
@@ -10,11 +11,33 @@ import ConditionalRender from './components/ConditionalRender'
 import ListRender from './components/ListRender'
 import ManageData from './components/ManageData'
 import ShowUserName from './components/ShowUserName'
+import Fragment from './components/Fragment'
+import Container from './components/Container'
+import ExecuteFunction from './components/ExecuteFunction'
+import Message from './components/Message'
+import ChangeMessageState from './components/ChangeMessageState'
+
 
 
 function App() {
 
   const name = 'Joaninha'
+
+  const cars = [
+    { id: 1, brand: 'Ferrari', color: 'Amarela', newCar: true, km: 0 },
+    { id: 2, brand: 'KIA', color: 'Branco', newCar: false, km: 34343 },
+    { id: 3, brand: 'Renault', color: 'Azul', newCar: false, km: 234 },
+  ]  
+
+  function showMessage() {
+    console.log('Evento do componente pai.')
+  }
+
+  const [message, setMessage] = useState('')
+
+  const handleMessage = (msg) => {
+    setMessage(msg)
+  }
 
   return (
     <div className="App">
@@ -40,6 +63,23 @@ function App() {
       <ShowUserName name={name} />
       {/* destructuring */}
       <CarDetails brand='VW' km={100000} color='Azul'/>
+      {/* reaproveitamento de componentes */}
+      <CarDetails brand='Ford' color='vermelha' km={0} newCar={true} />
+      <CarDetails brand='Fiat' color='branco' km={4500} newCar={false} />
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails key={car.id} brand={car.brand} color={car.color} km={car.km} newCar={car.newCar}/>
+      ))}
+      <Fragment />
+      {/* children */}
+      <Container myValue='testing'>
+        <p>E este é o conteúdo.</p>
+      </Container>
+      {/* criando uma função */}
+      <ExecuteFunction myFunction={showMessage}/>
+      {/* state lift */}
+      <Message msg={message}/>
+      <ChangeMessageState handleMessage={handleMessage}/>
     </div>
   );
 }
