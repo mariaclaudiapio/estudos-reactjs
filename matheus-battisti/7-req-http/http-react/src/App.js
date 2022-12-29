0a2c78f4dcc6276dd1e0f5ee85ec7316d2796aac
mariaclaudiapio <mariaclaudiapio@gmail.com>
2022-12-29 11:03:43 -0300

    requisições HTTP com React

---
+++ matheus-battisti/7-req-http/http-react/src/App.js
@@ -0,0 +1,105 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+
+import { useFetch } from './hooks/useFetch'
+
+const url = 'http://localhost:3000/products/'
+
+function App() {
+
+  const [products, setProducts] = useState([])
+
+  const { data: items, httpConfig, loading, error } = useFetch(url)
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url)
+  //     const data = await res.json()
+  
+  //     setProducts(data)
+  //   }
+  //   fetchData()
+  // }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+    
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // })
+
+    // const addedProduct = await res.json()
+
+    // setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    // 5 - refatorando POST
+    httpConfig(product, 'POST')
+
+    setName('')
+    setPrice('')
+  }
+
+  // 8 - desafio 6
+  const handleRemove = (id) => {
+    httpConfig(id, 'DELETE')
+  }
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!error && (
+        <ul>
+          {items && items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+              <button onClick={() => handleRemove(product.id)}>Delete</button>          
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input 
+            type="text" 
+            value={name} 
+            name="name"
+            onChange={(e) => setName(e.target.value)}  
+            />
+          </label>
+          <label>
+            Preço:
+            <input 
+            type="number" 
+            value={price} 
+            name="price"
+            onChange={(e) => setPrice(e.target.value)}  
+            />
+          </label>
+          {/* 7 - state de loaading no POST */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          { !loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
