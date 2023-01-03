b7bd761b82aa39fe6351460d509e48b93371691b
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-01-03 11:56:28 -0300

    working on authentication

--- hora-de-codar/1-intro/src/App.jsx
+++ hora-de-codar/1-intro/src/App.jsx
@@ -4,11 +4,14 @@ import FirstComponent from './components/FirstComponent'
 import MyComponent from './components/MyComponent'
 import TemplateExpression from './components/TemplateExpression'
 import Events from './components/Events'
+import Data from './components/Data'
+
+import city from './assets/city.jpg'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" style={{ paddingBottom: '500px'}}>
       <div>
         <h2>1 - Introdução</h2>
         <h3>Hello World!!</h3>
@@ -21,6 +24,15 @@ function App() {
         <TemplateExpression />
         <MyComponent />
         <Events />
+        <hr />
+      </div>
+
+      <div>
+        <h2>3 - Avançando em React</h2>
+        {/* 1 - imagem em public */}
+        <img src="/img1.jpg" alt="Alvorada" />
+        <img src={city} alt="Cidade à noite" />
+        <Data />
         <hr />
       </div>
     </div>
