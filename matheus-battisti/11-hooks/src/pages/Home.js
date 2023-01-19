f430ada18121c0b954907bd6da15a915ea517f78
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-01-19 14:47:26 -0300

    Estudando Hooks:
    useReducer

--- matheus-battisti/11-hooks/src/pages/Home.js
+++ matheus-battisti/11-hooks/src/pages/Home.js
@@ -1,9 +1,12 @@
 import HookUseState from "../components/HookUseState";
+import HookUseReducer from "../components/HookUseReducer";
+
 
 const Home = () => {
     return (
         <div>
             <HookUseState />
+            <HookUseReducer />
         </div>
     );
 };
