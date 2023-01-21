b8fd363bb4317aaabfd8c6c9d50cac2d1b942d37
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-01-20 22:19:01 -0300

    Estudando Hooks:
    useContext

--- matheus-battisti/11-hooks/src/pages/Home.js
+++ matheus-battisti/11-hooks/src/pages/Home.js
@@ -1,14 +1,23 @@
+import { useContext } from "react";
+
 import HookUseState from "../components/HookUseState";
 import HookUseReducer from "../components/HookUseReducer";
 // import HookUseEffect from "../components/HookUseEffect";
+import { SomeContext } from "../components/HookUseContext";
 
 
 const Home = () => {
+
+    const { contextValue } = useContext(SomeContext);
+
     return (
         <div>
             <HookUseState />
             <HookUseReducer />
             {/* <HookUseEffect /> */}
+            <h2>useContext</h2>
+            <p>Valor do contexto: {contextValue}</p>
+            <hr />
         </div>
     );
 };
