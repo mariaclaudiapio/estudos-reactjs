da7f3761cb80acaec0a3cfeca09ad3371c6dbe0d
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-01 21:45:36 -0300

    Hooks
    useRef

--- hora-de-codar/9-hooks/src/routes/Home.jsx
+++ hora-de-codar/9-hooks/src/routes/Home.jsx
@@ -4,6 +4,7 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 import { SomeContext } from "../components/HookUseContext";
+import HookUseRef from "../components/HookUseRef";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -16,6 +17,8 @@ const Home = () => {
       <HookUseEffect />
       <h2>useContext</h2>
       <p>Valor do contexto: {contextValue}</p>
+      <hr />
+      <HookUseRef />
       <hr />
     </div>
   );
